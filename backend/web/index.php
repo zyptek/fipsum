d2969ffa8d458b1983091bd9c9c61cf153d882d4
ShipShape-Development <paranoidx44@shipshape.cl>
2024-12-05 12:06:27 -0300

    second commit

---
+++ backend/web/index.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', '1'); // Mostrar errores en pantalla
+ini_set('display_startup_errors', '1'); // Mostrar errores de inicio de PHP
+error_reporting(E_ALL); // Reportar todos los tipos de errores
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../common/config/bootstrap.php';
+require __DIR__ . '/../config/bootstrap.php';
+
+$config = yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../config/main-local.php'
+);
+
+(new yii\web\Application($config))->run();
